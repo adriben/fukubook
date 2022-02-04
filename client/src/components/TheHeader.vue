2ee0c451fd376a99d72198629d056c306b6c557d
Adrien Benete <adrbenete@gmail.com>
2022-02-04 16:11:30 +0900

    ajout du front

---
+++ client/src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div >
+    <h1>Fukuobook</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TheHeader></TheHeader>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TheHeader from '@/components/TheHeader.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    TheHeader
+  }
+}
+</script>
