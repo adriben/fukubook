40143b65275ee747f9050bc85ead5eae5c2fb338
Adrien Benete <adrbenete@gmail.com>
2022-02-08 13:59:33 +0900

    debut de la creation du store pour centraliser les donnes de l user

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,37 @@
 import { createStore } from 'vuex'
+// import router from '../router/index';
+
+const axios = require('axios');
+const instance = axios.create({
+  baseUrl: "http://localhost:5000/api"
+})
+
+// let user = "";
 
 export default createStore({
   state: {
+    // user: {
+    //   userId = user.id,
+    //   username = user.username
+    // }
   },
   mutations: {
+    logUser: async function (state, user) {
+      state.user = {};
+      state.user.userId = user.id;
+      state.user.username = user.username;
+    }
+
   },
   actions: {
+
+    createAccount: async({ commit }, userInfos) => {
+      commit;
+      instance.post('/signup', userInfos)
+      .then((response) => {
+        console.log(response);
+      })
+    }
   },
   modules: {
   }
