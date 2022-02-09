afa8ebfece73f2d216e910a0349c56ae7fc4c926
Adrien Benete <adrbenete@gmail.com>
2022-02-09 16:39:18 +0900

    creation de la fonction pour enregistrer le nouvel user dans la DB

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,38 +1,52 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 // import router from '../router/index';
 
-const axios = require('axios');
+const axios = require("axios");
 const instance = axios.create({
-  baseUrl: "http://localhost:5000/api"
-})
+  baseUrl: "http://localhost:5000/api",
+});
 
-// let user = "";
+let user = -1;
+if (!user) {
+  //if there is nothing in local storage then the default userID is -1
+  user = {
+    userId: -1,
+    token: "",
+  };
+} else {
+  try {
+    instance.defaults.headers.common["Authorization"] = user.accessToken;
+  } catch (ex) {
+    user = {
+      userId: -1,
+      token: "",
+    };
+  }
+}
 
 export default createStore({
   state: {
-    // user: {
-    //   userId = user.id,
-    //   username = user.username
-    // }
+    status: "",
+    user: {
+      userId: user.id,
+      username: user.username,
+      token: user.accessToken,
+    },
   },
   mutations: {
     logUser: async function (state, user) {
       state.user = {};
       state.user.userId = user.id;
       state.user.username = user.username;
-    }
-
+    },
   },
   actions: {
-
-    createAccount: async({ commit }, userInfos) => {
+    createAccount: async ({ commit }, userInfos) => {
       commit;
-      instance.post('/signup', userInfos)
-      .then((response) => {
+      instance.post("http://localhost:5000/api/auth/signup", userInfos).then((response) => {
         console.log(response);
-      })
-    }
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- client/src/views/SignIn.vue
+++ client/src/views/SignIn.vue
@@ -1,120 +1,113 @@
 <template>
-<div>
-<TheHeader />
-<div>
-
-
-     <div class="card mt-5 signupCard">
-
-      <h2>Sign-up</h2>
-      <form class="card-body">
-        <label for="email">Your email</label>
-        <input
-          type="email"
-          placeholder="Email address"
-          v-model="email"
-          name="email"
-          id="email"
-          required
-        />
-        <label for="usernameSignup">Your username</label>
-        <input
-          type="text"
-          placeholder="Username"
-          v-model="username"
-          name="usernameSignup"
-          id="usernameSignup"
-          required
-        />
-        <label for="passwordSignup">Password</label>
-        <input
-          type="password"
-          placeholder="Password"
-          v-model="password"
-          name="passwordSignup"
-          id="passwordSignup"
-        />
-        <label for="confirmePassword">Confirm password</label>
-        <input
-          type="password"
-          placeholder="Confirm password"
-          v-model="confirmedPassword"
-          name="confirmePassword"
-          id="confirmePassword"
-        />
-        <button class="btn btn-success mt-3" @click="signIn">Sign-up</button>
-      </form>
-      <p class="errorMessage">{{ errorMessage }}</p>
-      <router-link to="/" class="link"
-        >Already an account? Click here to login
-      </router-link>
+  <div>
+    <TheHeader />
+    <div>
+      <div class="card mt-5 signupCard">
+        <h2>Sign-up</h2>
+        <form class="card-body">
+          <label for="email">Your email</label>
+          <input
+            type="email"
+            placeholder="Email address"
+            v-model="email"
+            name="email"
+            id="email"
+            required
+          />
+          <label for="usernameSignup">Your username</label>
+          <input
+            type="text"
+            placeholder="Username"
+            v-model="username"
+            name="usernameSignup"
+            id="usernameSignup"
+            required
+          />
+          <label for="passwordSignup">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            v-model="password"
+            name="passwordSignup"
+            id="passwordSignup"
+          />
+          <label for="confirmePassword">Confirm password</label>
+          <input
+            type="password"
+            placeholder="Confirm password"
+            v-model="confirmedPassword"
+            name="confirmePassword"
+            id="confirmePassword"
+          />
+          <button class="btn btn-success mt-3" @click="signIn">Sign-up</button>
+        </form>
+        <p class="errorMessage">{{ errorMessage }}</p>
+        <router-link to="/" class="link"
+          >Already an account? Click here to login
+        </router-link>
+      </div>
     </div>
-    
-</div>
-</div>
-
+  </div>
 </template>
 
 <script>
-import TheHeader from "../components/TheHeader.vue"
+import TheHeader from "../components/TheHeader.vue";
 
 export default {
-    name: 'SignIn',
-    components: {
-        TheHeader
-    },
-    data() {
-        return{
-            email: '',
-            username: '',
-            password: '',
-            confirmedPassword: '',
-            errorMessage: '',
-        }
-    },
-    methods: {
-        signIn: async function(event){
-            event.preventDefault();
-            let validEmail = new RegExp(
+  name: "SignIn",
+  components: {
+    TheHeader,
+  },
+  data() {
+    return {
+      email: "",
+      username: "",
+      password: "",
+      confirmedPassword: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    signIn: async function (event) {
+      event.preventDefault();
+      let validEmail = new RegExp(
         /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
-      if(!validEmail.test(this.email)){
-          this.errorMessage = "Please input a valid email address"
-      }else if(this.password.length < 5) {
-          this.errorMessage = 'The password must contain at least 5 characters'
-
+      if (!validEmail.test(this.email)) {
+        this.errorMessage = "Please input a valid email address";
+      } else if (this.password.length < 5) {
+        this.errorMessage = "The password must contain at least 5 characters";
       } else if (this.username.length < 2) {
         this.errorMessage = "The username must contain at leat 2 characters";
-      }else if (
+      } else if (
         this.username != "" &&
         this.email != "" &&
         this.password != "" &&
         this.confirmedPassword === this.password
-      ){
-          console.log(this.username);
+      ) {
+        console.log(this.username);
+        this.$store
+        .dispatch("createAccount", {
+          email: this.email,
+          username: this.username,
+          password: this.password
+        })
       }
-        }
-    }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.signupCard {
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  padding: 2rem;
 
-.signupCard{
+  form {
     display: flex;
     flex-direction: column;
-    width: fit-content;
-    padding: 2rem;
-
-
-    form{
-        display: flex;
-        flex-direction: column;
-        
-    }
-
+  }
 }
-
 </style>
-
-
